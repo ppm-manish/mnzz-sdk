807e99b936816be8d34bd43c38b2c3d72c5be339
ppm-manish <manish.kumar@paypermint.in>
2020-03-06 14:52:13 +0530

    v1.0.2 try2 - added log in push

--- push.sh
+++ push.sh
@@ -12,15 +12,21 @@ commit_files() {
     echo "commiting files"
   git checkout master
   git add -f releases/Mnzz-iOS-$VERSION.tar.gz
+    echo "git status before commit"
   git status
   git commit --message "Upload framework $VERSION"
+    echo "git status after commit"
+  git status
+    echo "git log after commit"
   git log
 }
 
 upload_files() {
     echo "uploading files"
   git remote add origin https://${TOKEN}@github.com/ppm-manish/mnzz-ios.git > /dev/null 2>&1
+    echo "git remote set"
   git push --set-upstream origin master 
+    echo "git log after push"
     git log
 }
 
