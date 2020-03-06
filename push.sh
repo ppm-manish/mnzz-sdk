a45f04b44d0b09ff97da28b9e26daec35b8dac35
ppm-manish <manish.kumar@paypermint.in>
2020-03-06 17:53:12 +0530

    v1.0.3

--- push.sh
+++ push.sh
@@ -1,33 +1,23 @@
 #!/bin/sh
-VERSION=1.0.2
-echo "pushing $VERSION"
+VERSION=1.0.3
 
 setup_git() {
-    echo "setting up git"
   git config --global user.email "manish.kumar@paypermint.in"
   git config --global user.name "ppm-manish"
 }
 
 commit_files() {
-    echo "git clone mnzz-ios"
-
     git clone https://${TOKEN}:x-oauth-basic@github.com/ppm-manish/mnzz-ios.git
     mkdir -p mnzz-ios/Framework
     rm mnzz-ios/Framework/*
-    cp releases/Mnzz-iOS-$VERSION.tar.gz mnzz-ios/Framework/Mnzz-iOS-$VERSION.tar.gz
+    cp -rf universal/Mnzz.framework mnzz-ios/Framework/Mnzz.framework
     cd mnzz-ios
     
-    echo "commiting files"
-    git remote -v
     git checkout master
-    git add Framework/Mnzz-iOS-$VERSION.tar.gz
-    echo "git status before commit"
-    git status
+    git add Framework/Mnzz.framework
+    
     git commit --message "Upload framework $VERSION"
-    echo "git status after commit"
-    git status
-    echo "git log after commit"
-    git log
+    
     git push -u origin master 
 }
 
