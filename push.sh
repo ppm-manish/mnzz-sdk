6f6cc91da396292dc9680db7f03522f9dd432fdc
Manish Kumar <51481703+ppm-manish@users.noreply.github.com>
2020-06-05 15:30:40 +0530

    Update push.sh

--- push.sh
+++ push.sh
@@ -7,16 +7,24 @@ setup_git() {
 }
 
 commit_files() {
+    pwd
     git clone https://${TOKEN}:x-oauth-basic@github.com/ppm-manish/mnzz-ios.git
+    ls -a
     mkdir -p mnzz-ios/Framework
     rm mnzz-ios/Framework/*
     cp -rf universal/Mnzz.framework mnzz-ios/Framework/Mnzz.framework
-    cd mnzz-ios
+    ls mnzz-ios
+    ls mnzz-ios/Framework
     
+    cd mnzz-ios
+    pwd
+    ls -a
     git checkout master
     git add Framework/Mnzz.framework
+    git status
     
     git commit --message "Upload framework $VERSION"
+    git status
     
     git push -u origin master 
 }
